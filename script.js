4807857c2c31d46a69318aa0f4fc454030517980
osintnull <bolsbabka@gmail.com>
2026-04-30 17:02:21 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const elements = document.querySelectorAll(
-  '.service-card, .process-card, .intro, .contact'
+  '.service-card, .pricing, .process-card, .intro, .contact'
 );
 
 const observer = new IntersectionObserver((entries) => {
